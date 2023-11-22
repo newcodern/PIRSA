9417e64625c8f5c9a1185b7ec218d482968422c4
Miskiyanto <mmiskiyanto97@gmail.com>
2023-11-22 18:06:31 +0700

    edit login

--- resources/views/auth/auth_page.blade.php
+++ resources/views/auth/auth_page.blade.php
@@ -1,6 +1,14 @@
 @include('include/items')
-
   <main>
+  <style>
+    body {
+      background-image: url('NiceAdmin/assets/img/bg_mem.webp');
+      background-repeat: no-repeat;
+      /* background-attachment: fixed;   */
+      background-size: cover;
+    }
+  </style>
+  
     <div class="container">
 
       <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
@@ -9,7 +17,7 @@
             <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
               <div class="d-flex justify-content-center py-4">
-                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                <a href="#" class="logo d-flex align-items-center w-auto">
                   <img src="{{asset('NiceAdmin/assets/img/logo.svg')}}" alt="">
                   <!-- <span class="d-none d-lg-block">NiceAdmin</span> -->
                 </a>
